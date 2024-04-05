27f9ea4ec17df31c80dcc6f4cccd334544f6384d
jayash260 <123286478+jayash260@users.noreply.github.com>
2024-04-05 07:18:30 +0000

    variabls

---
+++ 01_basic/variabls.js
@@ -0,0 +1,14 @@
+const accountId =12345
+let accountEmaill ="bhosalejayash@gmail.com"
+var Accountpassword="1234"
+accountCity="Jaipur"
+let accountstate;
+
+//accountId =2
+accountEmaill="ssss@ol"
+accountCity="nira"
+Accountpassword="1111"
+
+
+console.table([accountEmaill,accountId,accountCity,accountstate,Accountpassword]) 
+console.log(accountId);
